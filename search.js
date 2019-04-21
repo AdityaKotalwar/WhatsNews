d1231a58761de402eb74bf180dfd5078029f8375
pshishod <33633060+pshishod@users.noreply.github.com>
2019-04-21 19:36:10 -0400

    added and fixed main page

---
+++ search.js
@@ -0,0 +1,18 @@
+function func(){
+
+    var searchinput = document.getElementById("searchview")
+    var input = searchinput.value.toUpperCase();
+	var list = document.getElementById("list");
+	var innerlist = list.getElementsByTagName("li");
+	for(var i = 0; i < innerlist.length; i++){
+	    var a = innerlist[i].getElementsByTagName("a")[0];
+        var txtValue = a.textContent;
+        if (txtValue.toUpperCase().indexOf(input) != -1) {
+            innerlist[i].style.display = "block";
+        } else {
+            innerlist[i].style.display = "none";
+        }
+
+	}
+
+ }
