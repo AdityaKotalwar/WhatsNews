b1898de9c017f3e26c75f97f9ef1ec5924a59da2
jrushank <32016374+jrushank@users.noreply.github.com>
2019-04-23 19:33:25 -0400

    redirection not working

--- app.js
+++ app.js
@@ -12,33 +12,31 @@ var url = "mongodb+srv://whatsnews:alishamc@cluster0-ndzcp.mongodb.net/test?retr
 app.use(express.static(path.join('css', 'public')));
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
+app.get('/', function(req,res){
     console.log("GET");
-    res.sendFile(path.join(__dirname,'signin.html'));
-    
+    res.redirect('/signin');
+    // res.sendFile(path.join(__dirname,"signin.html"));
+});
+// app.get('/signin', (req, res)=>{
+//     console.log("GETsignin");
+//     //res.redirect('/main');
+//     // console.log(req);
+//     // console.log(req._parsedOriginalUrl.path);
+
+//     res.sendFile(path.join(__dirname,"signin.html"));
+// });
+
+app.get('/signup', (req, res)=>{
+    console.log("GETsignup");
+    res.sendFile(path.join(__dirname,'signup.html'));
 });
 
-app.get('/main', (req, res)=>{
+app.get('/main', function(req, res){
     console.log("GETergergerge");
     res.sendFile(path.join(__dirname,'main.html'));
-})
-
-// app.get('/auth',(req,res)=>{
-//     const userInput = req.body;
-//     MongoClient.connect(url, function(err, client){
-//         const db = client.db("mydb");
-//         var cursor = db.collection("users").findOne({email : userInput.email} & {password : userInput.password});
-//         if(cursor){
-//             res.sendFile(path.join(__dirname,'main.html'));
-//         }
-//     });
-// });
-
-// app.get('/signup',(req,res)=>{
-//     console.log("GET");
-//     res.sendFile(path.join(__dirname,'signup.html'));
-    
-// });
+    console.log("GETergergerge");
+    //res.sendFile(path.join(__dirname,'main.html'));
+});
 
 app.post('/signup', function(req,res, next){
     // Document to be inserted
@@ -55,40 +53,43 @@ app.post('/signup', function(req,res, next){
                 dbo.collection("users").insertOne(userInput, function(err, res1) {
                     if (err) throw err;
                     console.log("1 document inserted");
-                    // res.sendFile(path.join(__dirname,'main.html'));
+                    //return res.sendFile(path.join(__dirname,'/main'));
+                    res.redirect('localhost:7098/main');
                     db.close();
                 });
             }
         });
         
       });
-      console.log("yassasasasasa")
 });
+
+app.get('/signin', function(req, res, next){
+    console.log("GETsignin")
+    res.sendFile(path.join(__dirname,'signin.html'));
+})
+    
+
 
 app.post('/signin', function(req, res, next){
     // Document to be inserted
-
+    console.log("POSTsignin");
     const userInput = req.body;
     MongoClient.connect(url, { useNewUrlParser: true}, function(err, db) {
         if (err) throw err;
         var dbo = db.db("mydb");
-
         var document = dbo.collection("users").findOne({email : userInput.email, password : userInput.password}, function(err,resi){
             if(resi){
                 console.log(resi);
-                res.sendFile(path.join(__dirname,'main.html'));
-                // dbo.collection("users").insertOne(userInput, function(err, res1) {
-                //     if (err) throw err;
-                //     console.log("1 document inserted");
-                //     // res.sendFile(path.join(__dirname,'main.html'));
-                //     db.close();
-                // });
+                return res.redirect('/main');
+                // return res.sendFile(path.join(__dirname,'/main'));
+                //    return res.redirect('/main'); 
             }
+            
         });
-        
-      });
-      console.log("yassasasasasa")
+    });
+    
 });
+        
 
 db.connect((err)=>{
     // If err unable to connect to database
@@ -101,10 +102,27 @@ db.connect((err)=>{
     // Start up our Express Application
     // And listen for Request
     else{
-        app.listen(3090,()=>{
-            console.log('connected to database, app listening on port 3090');
+        app.listen(7098,()=>{
+            console.log('connected to database, app listening on port 7098');
         });
     }
 });
+
+//==================================================================
+
+// var express = require('express');
+// var path = require('path');
+// var bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+// var url = "mongodb+srv://whatsnews:alishamc@cluster0-ndzcp.mongodb.net/test?retryWrites=true";
+// var authRoutes = require('./routes/index');
+// var app = express();
+
+// mongoose.connect(url, {
+//     useMongoClient = true
+// });
+
+
 
 
