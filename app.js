70f04d9cb41e48cc44b0826671e908f7ad5a55fe
pshishod <33633060+pshishod@users.noreply.github.com>
2019-04-24 04:22:39 -0400

    heroku hosting changes

--- app.js
+++ app.js
@@ -179,7 +179,7 @@ db.connect((err)=>{
     // Start up our Express Application
     // And listen for Request
     else{
-        app.listen(7100,()=>{
+        app.listen(process.env.PORT || 7100,()=>{
             console.log('connected to database, app listening on port 7100');
         });
     }
